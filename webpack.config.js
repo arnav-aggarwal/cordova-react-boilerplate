a425c1df66e10e0cdb730d8c8e9f106e3102df05
Lauren Settle <lauren.settle@thecontrolgroup.com>
2018-02-06 10:34:28 -0800

    working

--- webpack.config.js
+++ webpack.config.js
@@ -1,72 +1,82 @@
+const merge = require('webpack-merge');
 const path = require('path');
 const webpack = require('webpack');
-//simplifies creation of HTML files to serve webpack bundles
-//const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const config = {
-	//base directory/absolute path
-	context: path.resolve(__dirname, 'cordova-react-boilerplate/app'),
-	entry: {
-		app: path.join(__dirname, 'app/index.js')
-	},
+
+const common = {
+	entry: path.join(__dirname, 'app'),
 	output: {
 		path: path.join(__dirname, 'www'),
 		filename: 'app.js'
 	},
-	devServer: {
-		port: 1200,
-		//tell the server where to serve static content from
-		//contentBase: path.resolve(__dirname, 'dist/assets/media'),
-		//hot module replacement
-		hot: true,
-		//show running processes in console
-		progress: true
+	node: {
+		fs: 'empty'
 	},
-	//just in case we need this for error reporting
-	devtool: 'source-map',
 	module: {
-		loaders: [
-			{
-				test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
-				loader: 'babel-loader',
-				query: {
-					presets: ['es2015', 'react']
-				}
-			}
-		],
 		rules: [{
-			test:  /\.(js|jsx)$/,
+			test: /\.jsx?$/,
 			loader: 'babel-loader?cacheDirectory',
 			exclude: '/node_modules/',
 			include: path.join(__dirname, 'app')
-		},{
+		}, {
+			test: /\.(svg)$/,
+			loader: 'raw-loader'
+		}, {
 			test: /\.less$/,
 			use: [
 				'style-loader',
 				'css-loader',
 				'less-loader'
 			]
-		},{
+		}, {
 			test: /\.css$/,
 			use: [
 				'style-loader',
 				'css-loader'
 			]
-		},
-		{
+		}, {
 			test: /\.(png|jpg|gif)$/,
 			loader: 'url-loader'
 		}]
+	},
+	cache: true
+};
+
+module.exports = merge(common, {
+	devtool: 'cheap-module-eval-source-map',
+	devServer: {
+		contentBase: path.join(__dirname, 'www'),
+		port: 1200,
+		historyApiFallback: true,
+		watchOptions: {
+			aggregateTimeout: 300,
+			poll: 1000
+		},
+		proxy: {
+			'/remove_dev_404s': {
+				target: 'goodbye_dev_404s',
+				secure: false,
+				bypass: (req, res) => {
+					if(req.url === '/cordova.js' || req.url === '/favicon.ico') {
+						// end the response quick so we don't get 404's
+						res.status(200).send('');
+					}
+				}
+			}
+		},
+		hot: true,
+		inline: true,
+		progress: true,
+		stats: 'errors-only'
 	},
 	plugins: [
 		new webpack.LoaderOptionsPlugin({
 			debug: true
 		}),
-		new webpack.HotModuleReplacementPlugin()
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.ProvidePlugin({
+			$: 'jquery',
+			jQuery: 'jquery'
+		})
 	]
-};
-
-//we’re using node.js and webpack uses the modular pattern,
-	//we need to export the configuration object
-module.exports = config;
+});
